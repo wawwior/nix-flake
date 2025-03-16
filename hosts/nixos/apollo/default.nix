c4404d40364fe72ed922f3320721bb287f8ebbb3
wawwior <45405580+wawwior@users.noreply.github.com>
2025-03-16 16:40:49 +0100

    new host

---
+++ hosts/nixos/apollo/default.nix
@@ -0,0 +1,62 @@
+{
+  inputs,
+  lib,
+  ...
+}:
+{
+  imports = lib.flatten [
+
+    inputs.nixos-facter-modules.nixosModules.facter
+
+    ./filesystem.nix
+
+    (map lib.custom.fromTop [
+      "hosts/common/core"
+      "hosts/common/optional/services/openssh.nix"
+      "hosts/common/optional/services/cpufreq.nix"
+      "hosts/common/optional/services/gnome-keyring.nix"
+      "hosts/common/optional/services/thermald.nix"
+
+      # PONDER_THE_ORB: is this the best way to do this?
+      "hosts/common/optional/stylix/catppuccin-mocha"
+
+      "hosts/common/optional/audio.nix"
+      "hosts/common/optional/sddm.nix"
+      "hosts/common/optional/hyprland.nix"
+    ])
+  ];
+
+  facter.reportPath = ./facter.json;
+
+  hostSpec = {
+    hostName = "apollo";
+  };
+
+  networking = {
+    networkmanager.enable = true;
+    enableIPv6 = true;
+  };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  hardware.nvidia = {
+    open = true;
+    modesetting.enable = true;
+  };
+
+  system.stateVersion = "24.11";
+
+  console.useXkbConfig = true;
+
+  services.xserver = {
+    xkb = {
+      layout = "de";
+      variant = "nodeadkeys";
+    };
+  };
+}
---
+++ hosts/nixos/apollo/filesystem.nix
@@ -0,0 +1,7 @@
+{ ... }:
+{
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+  };
+}
