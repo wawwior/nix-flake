327b3e0f45fee389b2a77bc226b20e461c4230eb
wawwior <45405580+wawwior@users.noreply.github.com>
2025-03-17 18:44:14 +0100

    reduce apollo to minimal

--- hosts/nixos/apollo/default.nix
+++ hosts/nixos/apollo/default.nix
@@ -8,7 +8,8 @@
 {
   imports = lib.flatten [
 
-    inputs.nixos-facter-modules.nixosModules.facter
+    # TODO: FIX facter
+    # inputs.nixos-facter-modules.nixosModules.facter
 
     inputs.disko.nixosModules.disko
     (lib.custom.fromTop "hosts/common/disks/lvm-simple.nix")
@@ -26,12 +27,12 @@
     (map lib.custom.fromTop [
       "hosts/common/core"
       "hosts/common/optional/services/openssh.nix"
-      "hosts/common/optional/services/cpufreq.nix"
+      # "hosts/common/optional/services/cpufreq.nix"
       # "hosts/common/optional/services/gnome-keyring.nix"
-      "hosts/common/optional/services/thermald.nix"
+      # "hosts/common/optional/services/thermald.nix"
 
       # PONDER_THE_ORB: is this the best way to do this?
-      # "hosts/common/optional/stylix/catppuccin-mocha"
+      "hosts/common/optional/stylix/catppuccin-mocha"
 
       # "hosts/common/optional/audio.nix"
       # "hosts/common/optional/sddm.nix"
@@ -39,7 +40,10 @@
     ])
   ];
 
-  facter.reportPath = ./facter.json;
+  nixpkgs.hostPlatform = "x86_64-linux";
+
+  # TODO: FIX facter
+  # facter.reportPath = ./facter.json;
 
   hostSpec = {
     hostName = "apollo";
@@ -50,19 +54,18 @@
     enableIPv6 = true;
   };
 
-  services.xserver.videoDrivers = [ "nvidia" ];
 
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
-  hardware.nvidia = {
-    open = false;
-    modesetting.enable = true;
-    powerManagement.enable = true;
-    package = config.boot.kernelPackages.nvidiaPackages.latest;
-  };
+  # TODO: FIX nvidia
+  # services.xserver.videoDrivers = [ "nvidia" ];
+  # 
+  # hardware.graphics = {
+  #   enable = true;
+  # };
+  #
+  # hardware.nvidia = {
+  #   open = true;
+  #   modesetting.enable = true;
+  # };
 
   boot = {
     loader = {
