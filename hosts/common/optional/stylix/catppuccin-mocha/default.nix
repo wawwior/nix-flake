3b9772c5b49ba574e3f813434bd4fbf2fa11907d
wawwior <45405580+wawwior@users.noreply.github.com>
2025-05-26 00:13:18 +0200

    chore(stylix): cleaned up

--- hosts/common/optional/stylix/catppuccin-mocha/default.nix
+++ hosts/common/optional/stylix/catppuccin-mocha/default.nix
@@ -6,28 +6,6 @@
 }:
 let
   hostSpec = config.hostSpec;
-
-  mkTintedPkg =
-    base:
-    pkgs.stdenv.mkDerivation {
-      name = "tinted-schemes-${base}";
-      src = pkgs.fetchFromGitHub {
-        owner = "tinted-theming";
-        repo = "schemes";
-        rev = "a1bc2bd89e693e7e3f5764cfe8114e2ae150e184";
-        sha256 = "sha256-Hdk850xgAd3DL8KX0AbyU7tC834d3Lej1jOo3duWiOA=";
-      };
-
-      installPhase = ''
-        mkdir -p $out/share/themes
-        cp -r ${base}/* $out/share/themes/
-      '';
-    };
-
-  tinted-schemes = {
-    base16 = mkTintedPkg "base16";
-    base24 = mkTintedPkg "base24";
-  };
 in
 {
 
@@ -37,9 +15,14 @@ in
 
     enable = true;
 
-    base16Scheme = "${tinted-schemes.base24}/share/themes/catppuccin-mocha.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
 
     polarity = "dark";
+
+    image = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/zhichaoh/catppuccin-wallpapers/refs/heads/main/os/nix-black-4k.png";
+      sha256 = "sha256-HRZYeKDmfA53kb3fZxuNWvR8cE96tLrqPZhX4+z4lZA=";
+    };
 
     cursor = {
       package = pkgs.bibata-cursors;
@@ -61,15 +44,6 @@ in
   };
 
   home-manager.users."${hostSpec.username}".stylix = {
-
-    enable = true;
-
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-
-    image = pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/zhichaoh/catppuccin-wallpapers/refs/heads/main/os/nix-black-4k.png";
-      sha256 = "sha256-HRZYeKDmfA53kb3fZxuNWvR8cE96tLrqPZhX4+z4lZA=";
-    };
 
     iconTheme = {
       enable = true;
