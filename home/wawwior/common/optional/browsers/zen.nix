9cacd5f0954dce5fd192f4c48331a358e3ec0051
wawwior <45405580+wawwior@users.noreply.github.com>
2025-10-12 22:14:42 +0200

    feat(home): add firefox policies to zen

--- home/wawwior/common/optional/browsers/zen.nix
+++ home/wawwior/common/optional/browsers/zen.nix
@@ -17,6 +17,22 @@
 
       };
     };
+
+    policies = {
+      DisableAppUpdate = true;
+      DisableFeedbackCommands = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DontCheckDefaultBrowser = true;
+      NoDefaultBookmarks = true;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+    };
   };
 
   xdg.mimeApps =
