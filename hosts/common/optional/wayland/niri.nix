3be7f01fbbdd0354e77a0eb4afebef7d0c91d2f6
wawwior <45405580+wawwior@users.noreply.github.com>
2025-12-11 19:27:15 +0100

    feat: finish up niri config

--- hosts/common/optional/wayland/niri.nix
+++ hosts/common/optional/wayland/niri.nix
@@ -42,6 +42,11 @@ let
           }) set.prefix) set.suffix
         )
     );
+
+  screenshot = # sh
+    ''
+      grim -g "$(slurp)" - | wl-copy
+    '';
 in
 {
   imports = [ inputs.niri.nixosModules.niri ];
@@ -54,6 +59,8 @@ in
   environment.systemPackages = [
     pkgs.xwayland-satellite
     pkgs.wl-clipboard-rs
+    pkgs.grim
+    pkgs.slurp
   ];
   home-manager.users =
     with builtins;
@@ -66,6 +73,82 @@ in
 
           programs.wofi.enable = true;
 
+          programs.waybar = {
+            systemd.enable = true;
+            enable = true;
+            settings = [
+              {
+                layer = "top";
+                position = "top";
+                height = 36;
+
+                modules-left = [ "clock" ];
+                modules-center = [ "tray" ];
+                modules-right = [
+                  "wireplumber"
+                  "backlight"
+                  "memory"
+                  "cpu"
+                  "temperature"
+                  "battery"
+                ];
+
+                temperature = {
+                  hwmon-path-abs = "/sys/devices/platform/coretemp.0/hwmon";
+                  input-filename = "temp1_input";
+                  format = " {temperatureC}°C";
+                };
+                clock = {
+                  format = "{:%a %d-%m-%Y %R:%S}";
+                  interval = 1;
+                };
+                wireplumber = {
+                  format = "{icon} {volume}%";
+                  format-muted = " MUTE";
+                  format-icons = [
+                    ""
+                    ""
+                    ""
+                  ];
+                };
+                memory = {
+                  format = " {used}G";
+                };
+                cpu = {
+                  format = " {usage}%";
+                };
+                battery = {
+                  bat = "BAT0";
+                  format-charging = "󱐋{icon} {capacity}%";
+                  format-discharging = "{icon} {capacity}%";
+                  format-icons = [
+                    "󰁺"
+                    "󰁻"
+                    "󰁼"
+                    "󰁽"
+                    "󰁾"
+                    "󰁿"
+                    "󰂀"
+                    "󰂁"
+                    "󰂂"
+                    "󰁹"
+                  ];
+                };
+                backlight = {
+                  format = "󰃠 {percent}%";
+                };
+              }
+            ];
+
+            style = lib.mkAfter ''
+              /* css */
+                * {
+                  font-family: "DejaVuSansM Nerd Font Mono";
+                  font-size: 13px;
+                }
+            '';
+          };
+
           services.mako = {
             enable = true;
             settings = {
@@ -76,10 +159,21 @@ in
 
           programs.niri = {
             settings = {
-              outputs.main = {
-                enable = true;
-                inherit (config.hostSpec.display) name mode;
+              outputs = {
+                main = {
+                  enable = true;
+                  inherit (config.hostSpec.display) name mode;
+                  position = {
+                    x = 0;
+                    y = 0;
+                  };
+                  scale = 1;
+                };
               };
+
+              spawn-at-startup = [
+
+              ];
 
               prefer-no-csd = true;
 
@@ -96,6 +190,11 @@ in
                 default-column-width = {
                   proportion = 0.5;
                 };
+                preset-column-widths = [
+                  { proportion = 1. / 3.; }
+                  { proportion = 1. / 2.; }
+                  { proportion = 2. / 3.; }
+                ];
                 border = {
                   width = 2;
                 };
@@ -108,19 +207,11 @@ in
                 };
               };
 
-              workspaces = {
-                "00-scratchpad" = {
-                  name = "scratchpad";
-                };
-                "01-main" = {
-                  name = "main";
-                };
-              };
-
               screenshot-path = "~/media/images/screenshots/Screenshot+%Y-%m-%d+%H-%M-%S";
 
               environment = {
                 "NIXOS_OZONE_WL" = "1";
+                "ELECTRON_OZONE_PLATFORM_HINT" = "auto";
               };
 
               window-rules = [
@@ -152,13 +243,14 @@ in
                 {
 
                   "Mod+Q".action = close-window;
-                  "Mod+Return".action = fullscreen-window;
+                  "Mod+Shift+Return".action = fullscreen-window;
                   # "Mod+?".action = show-hotkey-overlay;
+                  "Mod+Shift+Escape".action = quit;
 
                   "Mod+Space".action = spawn "kitty";
                   "Mod+A".action = spawn "wofi" "--show" "drun";
 
-                  "Mod+Shift+S".action = screenshot;
+                  "Mod+Shift+S".action = spawn-sh screenshot;
 
                   "Mod+J".action = focus-window-or-workspace-down;
                   "Mod+K".action = focus-window-or-workspace-up;
@@ -168,8 +260,17 @@ in
                   "Mod+WheelScrollUp".action = focus-workspace-up;
 
                   "Mod+O".action = toggle-window-floating;
+                  "Mod+I".action = switch-preset-column-width;
+                  "Mod+Return".action = maximize-column;
 
                   "Mod+Escape".action = toggle-overview;
+                  "XF86AudioRaiseVolume".action = spawn-sh "wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.05+";
+                  "XF86AudioLowerVolume".action = spawn-sh "wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.05-";
+                  "XF86AudioMute".action = spawn-sh "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
+                  "XF86AudioMicMute".action = spawn-sh "wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle";
+
+                  "XF86MonBrightnessUp".action = spawn-sh "brightnessctl set 10%+";
+                  "XF86MonBrightnessDown".action = spawn-sh "brightnessctl set 10%-";
                 }
                 // (binds {
                   prefix = {
