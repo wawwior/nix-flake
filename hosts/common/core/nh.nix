d0e198736d23a6280d3683d0ef09ea1024d83f63
wawwior <45405580+wawwior@users.noreply.github.com>
2026-02-14 19:20:05 +0100

    feat: keep only 2 system profiles after clean

--- hosts/common/core/nh.nix
+++ hosts/common/core/nh.nix
@@ -1,9 +1,9 @@
-{ config, ... }:
+{ ... }:
 {
   programs.nh = {
     enable = true;
     clean.enable = true;
-    clean.extraArgs = "--keep 5 --keep-since 3d --nogcroots";
+    clean.extraArgs = "--keep 2 --keep-since 3d --nogcroots";
     # TODO: fix
     flake = "/home/wawwior/.nixos";
   };
