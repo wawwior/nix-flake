1b4a4621d7a22c71b96d21edf7e3244edd135ef0
wawwior <45405580+wawwior@users.noreply.github.com>
2026-09-10 01:37:38 +0200

    feat: add capabilities

---
+++ modules/aspects/core/capabilities.nix
@@ -0,0 +1,24 @@
+{ self, lib, ... }: {
+  flake.aspects.core = { ... }: {
+    nixos = { config, ... }: {
+      options.capabilities = lib.mkOption {
+        type = lib.types.attrsOf self.lib.types.anythingConcatLists;
+        default = { };
+        description = "capabilities to be defined and consumed by aspects";
+      };
+      config = {
+        _module.args.capabilities = config.capabilities;
+      };
+    };
+    home = { config, ... }: {
+      options.capabilities = lib.mkOption {
+        type = lib.types.attrsOf self.lib.types.anythingConcatLists;
+        default = { };
+        description = "capabilities to be defined and consumed by aspects";
+      };
+      config = {
+        _module.args.capabilities = config.capabilities;
+      };
+    };
+  };
+}
