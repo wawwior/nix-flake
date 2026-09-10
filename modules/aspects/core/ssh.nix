c485b9fcb952097301fd1f2b15e21f8ccb058ebe
wawwior <45405580+wawwior@users.noreply.github.com>
2026-09-10 22:08:31 +0200

    fix: ssh config

--- modules/aspects/core/ssh.nix
+++ modules/aspects/core/ssh.nix
@@ -1,16 +1,21 @@
 {
   flake.aspects.core = { ... }: {
+
     nixos = {
       services.openssh.enable = true;
     };
+
     home = { config, ... }: {
       services.ssh-agent.enable = true;
       programs.ssh = {
         enable = true;
 
+        # TODO: wait for upstream to default this
+        enableDefaultConfig = false;
+
         # TODO: identityFile
 
-        matchBlocks = {
+        settings = {
           "*" = {
             controlMaster = "auto";
             controlPath = "${config.home.homeDirectory}/.ssh/sockets/S.%r@%h:%p";
